0acf1968e505dedc092a35788cc2f8f3f32eef14
dkhongell <116841735+dkhongell@users.noreply.github.com>
2022-11-03 15:51:59 -0400

    Add files via upload

---
+++ l10.py
@@ -0,0 +1,11 @@
+#the author is kalena'
+my_get_method
+
+h={"s":1, "c":2, "e":9, "l":6,}
+h
+h.get("s",0)
+list={"bread":"true", "eggs":"true", "fruit":"false", "veggies":"false"}
+list
+list.get("bread","vegan")
+
+list.get("eggs", "animal product?")
